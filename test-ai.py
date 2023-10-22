97ebbf41c7c89a21abb514a9c6ced54a5fd53f58
Eric Liu <liueric390@gmail.com>
2023-10-22 00:41:45 -0400

    Made boards expand on window resize

--- test-ai.py
+++ test-ai.py
@@ -28,7 +28,8 @@ root = tkinter.Tk()
 root.title("Arrow AI")
 
 BOARD_SIZE = 400
-root.geometry(f"{BOARD_SIZE*2}x{BOARD_SIZE + 300}")
+root.geometry(f"{BOARD_SIZE*2}x{BOARD_SIZE + 200}")
+root.minsize(BOARD_SIZE*2, BOARD_SIZE + 200)
 
 # ===== create Bottom Frame =====
 bottom_frame = tkinter.Frame(root, width=BOARD_SIZE*2, height=200)
@@ -48,35 +49,43 @@ def reset():
 	click_board.reset()
 	solution_board.reset()
 	update_solution()
+	update_solved_label()
 reset_button = tkinter.Button(bottom_frame, text="Reset", font=("Helvetica", 20), padx=10, pady=10, command=reset)
 reset_button.pack(side=tkinter.LEFT)
 
 def scramble():
 	click_board.spin_array(np.random.randint(0, 4, (4, 4)))
 	update_solution()
+	update_solved_label()
 scramble_button = tkinter.Button(bottom_frame, text="Scramble", font=("Helvetica", 20), padx=10, pady=10, command=scramble)
 scramble_button.pack(side=tkinter.LEFT)
 
 # ===== create boards =====
 def create_board_frame(root, title, board_size, side):
-	frame = tkinter.Frame(root, width=board_size, height=board_size+100, border=1,
+	frame = tkinter.Frame(root, width=1, height=1,
 					   highlightbackground="black", highlightcolor="black", highlightthickness=1)
-	frame.pack(side=side)
+	frame.pack(side=side, fill='both', expand=True)
+	frame.columnconfigure(0, weight=1)
+	frame.rowconfigure(1, weight=1)
 	label = tkinter.Label(frame, text=title, font=("Helvetica", 20))
 	label.grid(row=0, column=0)
 	board_frame = tkinter.Frame(frame, width=board_size, height=board_size)
 	board_frame.grid(row=1, column=0, sticky="nesw")
 	return frame, board_frame
 
-left_frame, left_board_frame = create_board_frame(root, "Puzzle (click to spin)", BOARD_SIZE, tkinter.LEFT)
+top_frame = tkinter.Frame(root, width=1, height=1, pady=10)
+top_frame.pack(side=tkinter.TOP, fill='both', expand=True)
+
+left_frame, left_board_frame = create_board_frame(top_frame, "Puzzle (click to spin)", BOARD_SIZE, tkinter.LEFT)
 click_board = UIBoard([4, 4], left_board_frame, BOARD_SIZE, view_only=False, 
 							on_click=[update_solution, update_solved_label])
 
-right_frame, right_board_frame = create_board_frame(root, "Solution (click to apply)", BOARD_SIZE, tkinter.RIGHT)
+right_frame, right_board_frame = create_board_frame(top_frame, "Solution (click to apply)", BOARD_SIZE, tkinter.RIGHT)
 def apply_solution(location, value):
 	if value != 0:
 		click_board.spin(location, value)
 		update_solution()
+		update_solved_label()
 solution_board = UIBoard([4, 4], right_board_frame, BOARD_SIZE, display_strings=["0", "1", "2", "3"], view_only=True,
 								on_click=[apply_solution, update_solved_label])
 
